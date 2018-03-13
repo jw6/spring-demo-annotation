4ce614d38e2833e8a395e88fea966b3abd861d2f
Jay Wang <jaywang007@yahoo.com>
2018-03-13 16:48:14 -0500

    qualifier, constructor injection

--- src/com/code/springdemo/RESTFortuneService.java
+++ src/com/code/springdemo/RESTFortuneService.java
@@ -1,14 +1,26 @@
 package com.code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RESTFortuneService implements FortuneService {
 
+	private String[] data = new String[]{
+			"GET request",
+			"POST request",
+			"PUT request",
+			"DELETE request"
+	};
+	
+	Random rand = new Random();
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		int index = rand.nextInt(data.length);
+		
+		return data[index];
 	}
 
 }
--- src/com/code/springdemo/SkiingCoach.java
+++ src/com/code/springdemo/SkiingCoach.java
@@ -1,10 +1,22 @@
 package com.code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SkiingCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public SkiingCoach(
+			@Qualifier("RESTFortuneService")
+			FortuneService fortuneService) {
+		System.out.println("Testing from REST");
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Skiing practice today";
@@ -13,6 +25,6 @@ public class SkiingCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.fortuneService.getFortune();
 	}
 }
