6a302c53db3eb12aefb4a7d913283f2c759fb52d
Jay Wang <jaywang007@yahoo.com>
2018-03-14 16:39:40 -0500

    configure spring container using java config

---
+++ src/com/code/springdemo/SportConfig.java
@@ -0,0 +1,11 @@
+package com.code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.code.springdemo")
+public class SportConfig {
+	
+}
