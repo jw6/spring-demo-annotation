f483eb398b7727f14b17feaf16a04d8d05fadc3c
Jay Wang <jaywang007@yahoo.com>
2018-03-18 22:16:55 -0500

    define spring beans using java code

--- src/com/code/springdemo/SwimJavaConfigDemoApp.java
+++ src/com/code/springdemo/SwimJavaConfigDemoApp.java
@@ -3,14 +3,14 @@ package com.code.springdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class JavaConfigDemoApp {
+public class SwimJavaConfigDemoApp {
 	public static void main(String[] args) {
 
 		// read spring config class 
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class); 
 
 		// get the bean from spring container
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		Coach theCoach = context.getBean("swimCoach", Coach.class);
 
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
